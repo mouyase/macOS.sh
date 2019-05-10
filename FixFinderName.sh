4edc2a474fbc4e0d8c3f35a448df67108e89c202
某亚瑟 <mouyase@qq.com>
2019-05-10 10:56:27 +0800

    Add files via upload

---
+++ FixFinderName.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+touch ~/Desktop/.localized
+touch ~/Pictures/.localized
+touch ~/Documents/.localized
+touch ~/Downloads/.localized
+touch ~/Movies/.localized
+touch ~/Library/.localized
+touch ~/Applications/.localized
+touch ~/Music/.localized
+killall Finder
